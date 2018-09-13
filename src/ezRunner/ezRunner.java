e15102150d0ecec2541eee218b6263109c35f758
jkuharev <jkuharev@1041-mac-joerg.local>
2018-09-13 15:26:50 +0200

    batch file creation and time stamping added

--- src/ezRunner/ezRunner.java
+++ src/ezRunner/ezRunner.java
@@ -12,6 +12,8 @@ import java.util.*;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import de.mz.jk.jsix.libs.XFiles;
+import de.mz.jk.jsix.libs.XJava;
 import de.mz.jk.jsix.ui.TextWindowDragAndDropUI;
 import de.mz.jk.jsix.ui.TextWindowDragAndDropUI.FileActionListener;
 import de.mz.jk.jsix.utilities.Settings;
@@ -50,16 +52,25 @@ public class ezRunner implements ActionListener, FileActionListener
 	private String params = "";
 	// filter dragged files by a file filter, regular expression or substring
 	private String dndFileRule = "*.*";
-	String paramFileKey = "file";
-	String[] paramKeys = { paramFileKey };
+
+	Set<String> paramKeys = new TreeSet<String>( Arrays.asList( new String[] { "user" } ) );
+	String[] builtinKeys = "file,date,time,timestamp".split( "," );
+
+	String formatDate = "yyyyMMdd";
+	String formatTime = "HHmmss";
+	String formatTimeStamp = "yyyyMMdd-HHmmss";
+
 	private Map<String, String> paramValues = null;
 
 	private boolean runOnDropEvent = false;
 	private boolean resetFilesAfterExecution = false;
+	private boolean executeCommand = true;
+	private boolean createBatchFile = true;
+	private String batchFileNamePattern = "$timestamp_ezRunner.bat";
 
 	public static void main(String[] args)
 	{
-		ezRunner r = new ezRunner();
+		new ezRunner();
 	}
 
 	public ezRunner()
@@ -115,27 +126,48 @@ public class ezRunner implements ActionListener, FileActionListener
 	private void initConfig()
 	{
 		paramValues = new HashMap<String, String>();
-		executable = cfg.getStringValue( "executableCommand", "echo", false );
-		paramKeys = cfg.getStringValue( "executableParameterKeys", "file,user", false ).split( "," );
-		paramFileKey = cfg.getStringValue( "fileParameterKey", "file", false );
-		params = cfg.getStringValue( "executableParameters", "Hello $user! Thank you for dropping file: $file", false );
-		dndFileRule = cfg.getStringValue( "fileAcceptanceFilter", ".*", false );
+		executable = cfg.getStringValue( "exec.command", "echo", false );
+		String[] userKeys = cfg.getStringValue( "exec.params.userKeys", "user", false ).split( "," );
+		paramKeys.addAll( Arrays.asList( userKeys ) );
+
+		// show internal keys to the user
+		cfg.setValue( "exec.params.internalKeys", XJava.joinArray( builtinKeys, "," ) );
+		params = cfg.getStringValue( "exec.params", "Hello $user! Thank you for dropping file: $file", false );
+		dndFileRule = cfg.getStringValue( "app.droppableFileFilter", ".*", false );
 		
-		runOnDropEvent = cfg.getBooleanValue( "application.autoRunOnDropEvent", false, false );
-		resetFilesAfterExecution = cfg.getBooleanValue( "application.resetFilesAfterExecution", true, false );
+		runOnDropEvent = cfg.getBooleanValue( "app.autoRunOnDropEvent", false, false );
+		resetFilesAfterExecution = cfg.getBooleanValue( "app.autoResetFileQueue", true, false );
 		
+		executeCommand = cfg.getBooleanValue( "app.onRunEvent.executeCommand", true, false );
+		createBatchFile = cfg.getBooleanValue( "app.onRunEvent.createBatchFile", false, false );
+		batchFileNamePattern = cfg.getStringValue( "app.batchFile.namePattern", "./$timestamp_ezRunner.bat", false );
+		
+		formatDate = cfg.getStringValue( "exec.params.format.date", formatDate, false );
+		formatTime = cfg.getStringValue( "exec.params.format.time", formatTime, false );
+		formatTimeStamp = cfg.getStringValue( "exec.params.format.timeStamp", formatTimeStamp, false );
+
 		for(String key : paramKeys)
 		{
 			paramValues.put( key, cfg.getStringValue( key, "$" + key, false ) );
+		}
+
+		for ( String key : builtinKeys )
+		{
+			paramKeys.add( key );
+			paramValues.put( key, "$" + key );
 		}
 	}
 
 	/**
 	 * @return
 	 */
-	private String parseParams()
+	private String parseParams(String paramTemplate)
 	{
-		String pars = params;
+		paramValues.put( "time", XJava.timeStamp( formatTime ) );
+		paramValues.put( "date", XJava.timeStamp( formatDate ) );
+		paramValues.put( "timestamp", XJava.timeStamp( formatTimeStamp ) );
+
+		String pars = paramTemplate;
 		for ( String key : paramKeys )
 		{
 			String value = paramValues.getOrDefault( key, key );
@@ -147,22 +179,45 @@ public class ezRunner implements ActionListener, FileActionListener
 	/** run command line for every file in the list */
 	private void run()
 	{
+		File batchFile = null;
 		try
 		{
+			if (createBatchFile)
+			{
+				String fileName = parseParams( this.batchFileNamePattern );
+				batchFile = new File( fileName );
+				XFiles.writeFile( batchFile, "\n" );
+				System.out.println( "writing command line commands to batch file ... " );
+				System.out.println( "	" + batchFile.getAbsolutePath() );
+			}
+
 			if (dndFiles.size() < 1)
 			{
-				executeCommandLine();
+				String cmd = getCommandLine();
+				if (createBatchFile && batchFile.canWrite()) XFiles.writeFile( batchFile, cmd + "\n" );
+				executeCommand( cmd );
 			}
 			else
 			{
+				String cmdLines = "";
 				for ( File f : dndFiles )
 				{
-					paramValues.put( paramFileKey, f.getAbsolutePath() );
-					executeCommandLine();
+					paramValues.put( "file", f.getAbsolutePath() );
+					String cmd = getCommandLine();
+					cmdLines += cmd + "\n";
+					executeCommand( cmd );
 				}
+				if (createBatchFile && batchFile.canWrite()) XFiles.writeFile( batchFile, cmdLines );
+
 				// reset files
 				if (resetFilesAfterExecution || runOnDropEvent)
+				{
 					reset( false );
+				}
+				else
+				{
+					System.out.println( "DO NOT FORGET TO CLEAR THE QUEUE!" );
+				}
 			}
 		}
 		catch (Exception e)
@@ -172,12 +227,11 @@ public class ezRunner implements ActionListener, FileActionListener
 		}
 	}
 
-	private void executeCommandLine() throws Exception
+	private String getCommandLine() throws Exception
 	{
-		String args = parseParams();
+		String args = parseParams( params );
 		String cmd = executable + " " + args;
-		System.out.println( "executing command line:\n\t" + cmd );
-		executeCommand( cmd );
+		return cmd;
 	}
 
 	/** list files and show the preparsed command line */
@@ -195,11 +249,13 @@ public class ezRunner implements ActionListener, FileActionListener
 		{
 			System.out.println( "	EMPTY! Please drag and drop files matching the rule: " + dndFileRule );
 		}
+		String p = parseParams( params );
+
 		System.out.println( "Keys:" );
 		for ( String k : paramKeys )
 			System.out.println( "	" + k + ": " + paramValues.getOrDefault( k, k ) );
 
-		String p = parseParams();
+
 		System.out.println( "Command:\n\t" + executable );
 		System.out.println( "Arguments:\n\t" + p );
 	}
@@ -248,21 +304,30 @@ public class ezRunner implements ActionListener, FileActionListener
 	/** don't touch as we already handle everything in the other method */
 	@Override public void doSingleFileAction(File file){}
 
-	private int executeCommand(String command)
+	private int executeCommand(String cmd)
 	{
-		StringBuffer output = new StringBuffer();
-		Process p;
-		try
+		System.out.println( "command line:\n\t" + cmd );
+		if (executeCommand)
 		{
-			p = Runtime.getRuntime().exec( command );
-			redirectStream( p.getInputStream(), false );
-			redirectStream( p.getErrorStream(), true );
-			p.waitFor();
-			return p.exitValue();
+			System.out.println( "executing command ... " );
+			StringBuffer output = new StringBuffer();
+			Process p;
+			try
+			{
+				p = Runtime.getRuntime().exec( cmd );
+				redirectStream( p.getInputStream(), false );
+				redirectStream( p.getErrorStream(), true );
+				p.waitFor();
+				return p.exitValue();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch (Exception e)
+		else
 		{
-			e.printStackTrace();
+			System.out.println( "Execution of the command has been switched off by user settings!" );
 		}
 		return 1;
 	}
